863130e7c0e6455f8bf8ebe035528be97b6e1073
shamshod8052 <shamshodramazonov8052@gmail.com>
2025-06-16 12:06:18 +0500

    fix

--- bot/filters/admin_filter.py
+++ bot/filters/admin_filter.py
@@ -8,7 +8,7 @@ class AdminFilter(Filter):
     async def __call__(self, message: Message) -> bool:
         try:
             user = User.objects.get(telegram_id=message.from_user.id)
-            return user.is_staff
+            return user.is_tg_admin
         except User.DoesNotExist:
             ...
         return False
